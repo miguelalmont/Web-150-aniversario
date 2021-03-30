039bfdc56699c971df32d7a207633fe4a1257249
Miguel AM <miguelalmont87@gmail.com>
2021-03-30 13:49:26 +0200

    add routing children

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,18 @@ import { HimnoComponent } from './himno/himno.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 
 const routes: Routes = [
-  { path: 'home', component: SidenavComponent },
-  { path: 'registro', component: FormularioComponent },
-  { path: 'himno', component: HimnoComponent },
+  { path: 'home', component: SidenavComponent,
+    children: [
+      { path: 'registro', component: FormularioComponent },
+      { path: 'himno', component: HimnoComponent },
+    ]
+  },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   //{ path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
