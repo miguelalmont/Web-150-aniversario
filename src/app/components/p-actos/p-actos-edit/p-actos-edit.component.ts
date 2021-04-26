6d9bf0394eaa065c511a443fde1a70fd0c4de3e2
rararo13 <rararo13@gmail.com>
2021-04-26 18:07:00 +0200

    Pantalla P-Actos completa

--- src/app/components/p-actos/p-actos-edit/p-actos-edit.component.ts
+++ src/app/components/p-actos/p-actos-edit/p-actos-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
+
 @Component({
   selector: 'app-p-actos-edit',
   templateUrl: './p-actos-edit.component.html',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PActosEditComponent implements OnInit {
 
-  constructor() { }
+  newUserForm: FormGroup = this.fb.group({
+    title: new FormControl('',  [Validators.required, Validators.minLength(6)]),
+    description: new FormControl('',  [Validators.required, Validators.minLength(6)]),
+    category: new FormControl('',  [Validators.required, Validators.minLength(6)]),
+    date: new FormControl('',  Validators.required),
+    image: new FormControl('', Validators.required)
+  });
 
-  ngOnInit(): void {
+  user = {
+    title: this.newUserForm.get('title').value,
+    description: this.newUserForm.get('description').value,
+    category: this.newUserForm.get('category').value,
+    date: this.newUserForm.get('date').value,
+    image: this.newUserForm.get('image').value
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  get title() { return this.newUserForm.get('title').value; }
+
+  ngOnInit(): void {}
+
+  onFormSubmit(): void {
+    console.log('Name:' + this.newUserForm.get('title').value);
   }
 
 }
--- src/app/components/p-actos/p-actos-view/p-actos-view.component.ts
+++ src/app/components/p-actos/p-actos-view/p-actos-view.component.ts
@@ -1,15 +1,136 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatDialog} from '@angular/material/dialog';
+import { PActosFormComponent } from '../p-actos-form/p-actos-form.component';
+import { PActosEditComponent } from '../p-actos-edit/p-actos-edit.component';
+import { PActosDetailsComponent } from '../p-actos-details/p-actos-details.component';
+
+
+export interface ActoData {
+  title: string;
+  description: string;
+  category: string;
+  date: string;
+  image: string;
+}
+ 
+let usersData: ActoData[] = [
+  {
+    title: 'Saludo de la madre Yvonne',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    category: 'image caption',
+    date: 'image caption',
+    image: 'image caption',
+  },
+  {
+    title: 'Saludo de la madre Yvonne',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    category: 'image caption',
+    date: 'image caption',
+    image: 'image caption',
+  },
+  {
+    title: 'Saludo de la madre Yvonne',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    category: 'image caption',
+    date: 'image caption',
+    image: 'image caption',
+  },
+  {
+    title: 'Saludo de la madre Yvonne',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    category: 'image caption',
+    date: 'image caption',
+    image: 'image caption',
+  },
+  {
+    title: 'Saludo de la madre Yvonne',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    category: 'image caption',
+    date: 'image caption',
+    image: 'image caption',
+  },
+  {
+    title: 'Saludo de la madre Yvonne',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    category: 'image caption',
+    date: 'image caption',
+    image: 'image caption',
+  }
+]
 
 @Component({
   selector: 'app-p-actos-view',
   templateUrl: './p-actos-view.component.html',
   styleUrls: ['./p-actos-view.component.scss']
 })
-export class PActosViewComponent implements OnInit {
+export class PActosViewComponent implements AfterViewInit {
+  displayedColumns: string[] = ['title', 'description', 'category', 'date', 'image', 'actions'];
+  dataSource: MatTableDataSource<ActoData>;
+  users: ActoData[] = usersData;
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-  ngOnInit(): void {
+  constructor(public dialog: MatDialog) {
+
+    this.dataSource = new MatTableDataSource(this.users);
+    console.log(this.dataSource)
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.paginator._intl.itemsPerPageLabel = 'Por página';
+    this.paginator._intl.nextPageLabel = 'Siguiente página';
+    this.paginator._intl.previousPageLabel = 'Página anterior';
+    this.paginator._intl.getRangeLabel = 
+    (page: number, pageSize: number, length: number) => {
+      if (length === 0 || pageSize === 0) {
+        return `Sin registros`;
+      }
+      length = Math.max(length, 0);
+      const startIndex = page * pageSize;
+      // If the start index exceeds the list length, do not try and fix the end index to the end.
+      const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
+      return `${startIndex + 1} - ${endIndex} de ${length}`;
+    };
+    
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  createUserOnClick() {
+    const dialogRef = this.dialog.open(PActosFormComponent, { disableClose: true });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  editUserOnClick() {
+    const dialogRef = this.dialog.open(PActosEditComponent, { disableClose: true });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  detailsUserOnClick() {
+    const dialogRef = this.dialog.open(PActosDetailsComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+  
 }
