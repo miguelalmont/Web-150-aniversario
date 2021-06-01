b17a6fabeb3d2f7a0a766f5d075a014dafe0b0de
Migue <miguelalmont87@gmail.com>
2021-06-01 18:38:36 +0200

    new Interceptor

--- src/app/services/httpInterceptor.service.ts
+++ src/app/services/httpInterceptor.service.ts
@@ -2,8 +2,10 @@ import { baseUrl } from 'src/environments/environment';
 import { Injectable } from '@angular/core'; 
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http'; 
 import { Observable } from 'rxjs'; 
-import { map, finalize} from 'rxjs/operators'; 
+import { map, finalize, catchError} from 'rxjs/operators'; 
 import { AuthenticatorJwt } from './authenticatorJwt.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
 
 @Injectable({ 
   providedIn: 'root' 
@@ -14,40 +16,52 @@ export class HttpInterceptorService implements HttpInterceptor {
 
   constructor(private autenticadorJwt: AuthenticatorJwt) { }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    const token: string = this.autenticadorJwt.getJWT(); 
-    if (token) { 
-      request = request.clone({ headers: request.headers.set('Authorization', 'Bearer ' + token) }); 
+    const token: string = this.autenticadorJwt.getJWT();
+    // Check the token to add it to the headers
+    if (token) {
+      req = req.clone({ headers: req.headers.set('Authorization', 'Bearer ' + token) })
     }
 
-    // if (!request.headers.has('Content-Type')) { 
-    //   request = request.clone({ headers: request.headers.set('Content-Type', 'application/json; charset=utf-8') }); 
+    // Adds the headers
+    // if (!req.headers.has('Content-Type')) {
+    //   req = req.clone({ headers: req.headers.set('Content-type', 'application/json; charset=utf-8') })
     // }
+    // req = req.clone({ headers: req.headers.set('Accept', 'application/json') });
+    // req = req.clone({ headers: req.headers.set('Access-Control-Allow-Origin', '*') });
+    // req = req.clone({ headers: req.headers.set('Access-Control-Allow-Methods', 'DELETE, POST, GET, OPTIONS') });
+    // req = req.clone({ headers: req.headers.set('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With') });
 
-    // request = request.clone({ headers: request.headers.set('Accept', 'application/json;') });
-    // request = request.clone({ headers: request.headers.set('Access-Control-Allow-Origin', '*') });
+    // Creates the new url
+    const newUrl = { url: this.baseUrl + req.url };
+    req = Object.assign(req, newUrl);
+    const newUrlWithParams = { urlWithParams: this.baseUrl + req.urlWithParams };
+    req = Object.assign(req, newUrlWithParams);
 
-  
-    // request = request.clone({ headers: request.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, HEAD') });
-    // request = request.clone({ headers: request.headers.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Access-Control-Allow-Headers, Authorization, Content-Type, Accept') });
+    return next.handle(req).pipe(
+      map((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          console.log('event ---> ', event)
+        } return event;
+      }),
+      finalize(() => { })
 
-    const newUrl = {url: this.baseUrl + request.url}; 
-    request = Object.assign(request, newUrl); 
-    const newUrlWithParams = {urlWithParams: this.baseUrl + request.urlWithParams}; 
-    request = Object.assign(request, newUrlWithParams);
-
-    // console.log(request);
-    console.log(request.urlWithParams)
-
-    return next.handle(request).pipe( 
-      map((event: HttpEvent<any>) => { 
-        if (event instanceof HttpResponse) { 
-          console.log('event--->>>', event); // Si utilizas esta línea obtendrás un log de todas las respuestas http recibidas en tu app 
-        } return event; 
-      }), 
-      finalize(() => { 
-      }) 
+      // Controls all the errors from the diferents services
+    ).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMsg = '';
+        if (error.error instanceof ErrorEvent) {
+          console.log('this is client side error');
+          errorMsg = `Error: ${error.error.message}`;
+        }
+        else {
+          console.log('this is server side error');
+          errorMsg = `Error Code: ${error.status},  Message: ${error.message}`;
+        }
+        console.log(errorMsg);
+        return throwError(errorMsg);
+      })
     );
   }
 }
