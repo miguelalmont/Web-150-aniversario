3b48b7a79a58d3395559d8d4d63f4246241e751c
JoserusoDAM <joserusoalvarez@gmail.com>
2021-06-08 20:32:32 +0200

    Oracion details y edit

--- src/app/components/oracion/oracion-details/oracion-details.component.ts
+++ src/app/components/oracion/oracion-details/oracion-details.component.ts
@@ -1,52 +1,65 @@
 import { Component, OnInit, Inject, Input } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 import { Oracion } from 'src/app/models/models';
 
 @Component({
   selector: 'app-oracion-details',
   templateUrl: './oracion-details.component.html',
-  styleUrls: ['./oracion-details.component.scss']
+  styleUrls: ['./oracion-details.component.scss'],
 })
 export class OracionDetailsComponent implements OnInit {
-
-  @Input() oracionDetails: Oracion = {
-      titulo: '',
-      oracion: '',
-      enUso: 0
-  }
-
-  detailOracionForm: FormGroup;
-
+  @Input() oracionInput: Oracion = {
+    id: 0,
+    titulo: '',
+    oracion: '',
+    enUso: 0,
+  };
 
   newOracionForm: FormGroup = this.fb.group({
-    titulo: new FormControl('',  [Validators.required, Validators.minLength(6)]),
-    oracion: new FormControl('',  [Validators.required, Validators.minLength(6)]),
-    enUso: new FormControl('', Validators.required)
+    titulo: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    oracion: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    enUso: new FormControl('', Validators.required),
   });
 
   oracion = {
+    id: this.oracionInput.id,
     titulo: this.newOracionForm.get('titulo').value,
     oracion: this.newOracionForm.get('oracion').value,
-    enUso: this.newOracionForm.get('enUso').value
-  }
+    enUso: this.newOracionForm.get('enUso').value,
+  };
 
   constructor(private fb: FormBuilder, @Inject(MAT_DIALOG_DATA) public data) {
-    this.oracionDetails = data.row;
-    this.detailOracionForm = this.fb.group({
-      titulo: new FormControl(this.oracionDetails.titulo),
-      oracion: new FormControl(this.oracionDetails.oracion),
-      enUso: new FormControl(this.oracionDetails.enUso)
+    this.oracionInput = data.row;
+    this.newOracionForm = this.fb.group({
+      titulo: new FormControl(this.oracionInput.titulo),
+      oracion: new FormControl(this.oracionInput.oracion),
+      enUso: new FormControl(this.oracionInput.enUso),
     });
   }
 
-  get titulo() { return this.newOracionForm.get('titulo').value; }
+  get titulo() {
+    return this.newOracionForm.get('titulo').value;
+  }
+
+  checkUso: boolean;
 
   ngOnInit(): void {}
 
   onFormSubmit(): void {
-    console.log('Titulo:' + this.newOracionForm.get('titulo').value);
+    console.log(this.newOracionForm);
   }
 
+  checkEnUso(enUso: number): boolean {
+    return enUso === 0 ? false : true;
+  }
 }
--- src/app/components/oracion/oracion-edit/oracion-edit.component.ts
+++ src/app/components/oracion/oracion-edit/oracion-edit.component.ts
@@ -1,11 +1,12 @@
 import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Oracion } from 'src/app/models/models';
 import { OracionService } from 'src/app/services/oracion-service/oracion.service';
 import { Inject } from '@angular/core';
 import Swal from "sweetalert2";
+import { OracionViewComponent } from '../oracion-view/oracion-view.component';
 
 @Component({
   selector: 'app-oracion-edit',
@@ -37,7 +38,8 @@ export class OracionEditComponent implements OnInit {
     
   }
 
-  constructor(private fb: FormBuilder, @Inject(MAT_DIALOG_DATA) public data, private oracionService: OracionService) {
+  constructor(private fb: FormBuilder, @Inject(MAT_DIALOG_DATA) public data, private oracionService: OracionService,
+  public dialogRef: MatDialogRef<OracionViewComponent>) {
     this.oracionInput = data.row;
     this.newOracionForm = this.fb.group({
       titulo: new FormControl(this.oracionInput.titulo),
@@ -58,8 +60,14 @@ export class OracionEditComponent implements OnInit {
       enUso: this.newOracionForm.get('enUso').value
     }
     this.oracionService.editOracion(this.oracion).subscribe(
-      res => console.log("formulario editado"),
-      error => console.log(error)
+      res => {
+        console.log("formulario editado")
+        this.dialogRef.close();
+      },
+      error => {
+        console.log(error)
+        this.dialogRef.close();
+      }
     );
   }
 
