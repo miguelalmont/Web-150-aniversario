96a9d38ee30abe904c9b8513384ec6712ee4e75a
rararo13 <rararo13@gmail.com>
2021-06-10 13:48:05 +0200

    Servicios inyectados en oracion (acabado)

--- src/app/components/oracion/oracion-form/oracion-form.component.ts
+++ src/app/components/oracion/oracion-form/oracion-form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-
+import { OracionService } from 'src/app/services/oracion-service/oracion.service';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -25,14 +26,66 @@ export class OracionFormComponent implements OnInit {
     
   }
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private oracionService: OracionService) {}
 
   get titulo() { return this.newOracionForm.get('titulo').value; }
 
   ngOnInit(): void {}
 
   onFormSubmit(): void {
+    this.oracion = {
+      titulo: this.newOracionForm.get('titulo').value,
+      oracion: this.newOracionForm.get('oracion').value,
+      enUso: this.newOracionForm.get('enUso').value
+    }
     console.log('Name:' + this.newOracionForm.get('titulo').value);
+    this.oracionService.createOracion(this.oracion).subscribe(
+      response => {
+        console.log('Oracion insertada ', response)
+        Swal.fire({
+          title: '¿Estas seguro?',
+          text: "Vas a crear una Oracion",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          cancelButtonText: "Cancelar",
+          confirmButtonText: 'Crear'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            Swal.fire(
+              'Perfecto',
+              'Oracion creada correctamente',
+              'success'
+            )
+          }
+        })
+      },
+      error => {
+        console.error('Error ', error)
+        Swal.fire({
+          title: 'Error',
+          text: 'Hubo un error al crear',
+          icon: 'error',
+          cancelButtonColor: '#d33',
+          cancelButtonText: "Cerrar",
+        })
+      }
+    );
+  }
+
+  checkEnUso(enUso: number) {
+    if (enUso === 1)
+      return true;
+    else
+      return false;
+  }
+
+  unCheckEnUso(enUso: boolean) {
+    if (enUso)
+      return 1;
+    else
+      return 0;
   }
 
 }
